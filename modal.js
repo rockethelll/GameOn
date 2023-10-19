08840ff8bf2c4c7dcea18b24603b2bb87ea8cea2
rockethelll <51137055+rockethelll@users.noreply.github.com>
2023-10-19 14:38:13 +0200

    clean html, css, js. Add close modal JS

--- modal.js
+++ modal.js
@@ -1,23 +1,34 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const btnSignup = document.querySelectorAll(".btn-signup");
+const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".close");
+
+// Hide navbar on mobile and display burger menu dropdown
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  const myTopnav = document.getElementById("myTopnav");
+  if (myTopnav.className === "topnav") {
+    myTopnav.className += " responsive";
   } else {
-    x.className = "topnav";
+    myTopnav.className = "topnav";
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// close modal event
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+btnSignup.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+btnClose.addEventListener("click", closeModal);
 
 
